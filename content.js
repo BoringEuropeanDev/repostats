9fb4e05ba5217bf27825c3c3090976ac167ae6d7
Boring Euro Dev <245870614+BoringEuropeanDev@users.noreply.github.com>
2026-01-04 17:08:07 +0100

    Create content.js

---
+++ content.js
@@ -0,0 +1,7 @@
+// RepoStats - Content Script
+// Runs on GitHub pages to detect repo information
+
+console.log('RepoStats content script loaded');
+
+// Simple content script - just needs to exist
+// The real work happens in popup.js when user clicks the icon
